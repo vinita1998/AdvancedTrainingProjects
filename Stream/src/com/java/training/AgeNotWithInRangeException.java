36c7c81d0d1bdf8c8199a053d8503636cfa40318
Vinita <zugarevinita@gmail.com>
2022-03-26 10:48:07 +0530

    commit

---
+++ Stream/src/com/java/training/AgeNotWithInRangeException.java
@@ -0,0 +1,10 @@
+package com.java.training;
+
+public class AgeNotWithInRangeException extends Exception {
+	
+	     public String toString()
+	     {
+	          return ("Age is not between 15 and 21. please ReEnter the Age");
+	     }
+
+}
---
+++ Stream/src/com/java/training/NameNotValidException.java
@@ -0,0 +1,9 @@
+package com.java.training;
+
+public class NameNotValidException extends Exception {
+	public String validname()
+    {
+         return ("Please Enter Valid Name");
+    }
+
+}
