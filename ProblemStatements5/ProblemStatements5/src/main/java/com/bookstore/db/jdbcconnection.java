dbf3dcf6c1acf1108ee46a4cf49eb372dbd24427
Vinita <zugarevinita@gmail.com>
2022-03-28 22:30:23 +0530

    commit

---
+++ ProblemStatements5/ProblemStatements5/src/main/java/com/bookstore/db/jdbcconnection.java
@@ -0,0 +1,26 @@
+package com.bookstore.db;
+
+import java.sql.DriverManager;
+
+import org.eclipse.jdt.internal.compiler.ast.ReturnStatement;
+
+import com.mysql.jdbc.Connection;
+
+public class jdbcconnection {
+	private static Connection con;
+	
+	public static Connection getConnection() {
+		try {
+			if(con==null) {
+				Class.forName("com.mysql.jdbc.Driver");
+				con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/shopping cart for a bookstore","root","");
+			}
+			
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+}
